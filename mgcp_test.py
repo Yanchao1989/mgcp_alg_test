f2a964e142eaba034ffa3bfa25ac46332103aa92
Yanchao1989 <xycem@qq.com>
2015-05-09 21:26:13 +0800

    fix bugs

--- mgcp_test.py
+++ mgcp_test.py
@@ -4,13 +4,11 @@ import sys
 import random
 import re
 import traceback
+import time
 
 #globals
-messages=""
 gw_v6_mode=False
 udp_mode=False
-rtp_port_min=5000
-rtp_port_max=5000
 ca_mode = False
 
 ca_addr = ""
@@ -19,12 +17,14 @@ gw_addr = ""
 gw_port = 0
 gw_proto=""
 
+messages=""
+
 rtp_socket_list = [] # for fake rtp
 msg_values_dic = {}  # for message values replacement
 
 def usage():
     print(
-        """useage:
+        """usage:
         -f file  MGCP messages file
         -s       In ca mode
         """)
@@ -53,9 +53,6 @@ def read_block(block_name):
 def load_setting():
     global gw_v6_mode
     global udp_mode
-    global rtp_port_max
-    global rtp_port_min
-
     global ca_addr
     global ca_port
     global gw_addr
@@ -88,15 +85,8 @@ def load_setting():
                  msg_values_dic["gw_proto"] = gw_proto
              elif line.startswith("udp_mode="):
                  udp_mode = eval(line[9:])
-             elif line.startswith("rtp_port_min="):
-                 rtp_port_min = int(line[13:])
-                 msg_values_dic["rtp_port_min"] = rtp_port_min
-             elif line.startswith("rtp_port_max="):
-                 rtp_port_max = int(line[13:])
-                 msg_values_dic["rtp_port_max"] = rtp_port_max
              else:
                  msg_values_dic[line.split("=")[0]] = line.split("=")[1]
-
     except:
         print "message file error"
         print msg_values_dic
@@ -106,24 +96,17 @@ def load_setting():
 
 def rtp_fake():
     global rtp_socket_list
-    print "faking rtp/rtcp"
+    if len(rtp_socket_list) != 0:
+        print "faking rtp/rtcp"
     for s in rtp_socket_list:
         s[0].sendto("fake rtp/rtcp", s[1])
-
-def get_rtp_port():
-    global rtp_port_max
-    global rtp_port_min
-    return random.randrange(rtp_port_min, rtp_port_max, 2)
+        print "send fake rtp/rtcp to[%s]:%d"%s[1]
 
 def msg_preprocess(message):
     result = ""
-    rtp_port = get_rtp_port()
     for line in message:
         for key in msg_values_dic.keys():
-            if key == "rtp_port":
-                line = line.replace("$"+key, str(rtp_port))
-            else:
-                line = line.replace("$"+key, str(msg_values_dic[key]))
+            line = line.replace("$"+key, str(msg_values_dic[key]))
         result += line+"\n"
     return result
 
@@ -133,22 +116,41 @@ def ca_msg_preprocess(message):
 def gw_msg_preprocess(message):
     return msg_preprocess(message)
 
-def ca_process_msg(message, addr):
-    global ca_addr
-    global ca_port
+def get_rtp_addr(sdp_message):
+    addr=None
+    for line in sdp_message:
+        if line.startswith("c=IN IP"):
+            addr = line[9:].split(" ")[0]
+    return addr
+
+def get_rtp_ports(sdp_message):
+    ports = []
     p=re.compile("(?<=audio )\d+|(?<=video )\d+")
     for line in message:
-        ports = p.findall(line)
-        for port in ports:
-            rtp_port = int(port)
-            rtcp_port += 1
-            s_rtp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-            s_rtcp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-            s_rtp.bind((ca_addr,0))
-            s_rtcp.bind((ca_addr,0))
-            rtp_socket_list.append((s_rtp,(addr, rtp_port)))
-            rtp_socket_list.append((s_rtcp,(addr, rtcp_port)))
+        ports_str = p.findall(line)
+        for port in ports_str:
+            ports.append(int(port))
+    return ports
 
+def ca_process_msg(message, addr):
+    global ca_addr
+    global rtp_socket_list
+
+    rtp_dst_addr = get_rtp_addr(message)
+    if not rtp_dst_addr:
+        return
+    if dst_addr != addr:
+        print "Warnning: %s!=%s"%(dst_addr, ca_addr)
+
+    rtp_ports = get_rtp_ports(message)
+    for rtp_port in rtp_ports:
+        s_rtp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+        s_rtp.bind((ca_addr,0))
+        rtp_socket_list.append((s_rtp,(rtp_dst_addr, rtp_port)))
+        rtcp_port += 1
+        s_rtcp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+        s_rtcp.bind((ca_addr,0))
+        rtp_socket_list.append((s_rtcp,(rtp_dst_addr, rtcp_port)))
 
 def run_ca():
     global ca_addr
@@ -165,35 +167,32 @@ def run_ca():
         
 
     while True:
+        sent=0
+        received=0
+        at_received = received
         # Establish connection with gw.
         if not udp_mode:
             c, addr = s.accept()
-            msg = "%s:%d connected"%addr
+            msg = c.recv(1024)
         else:
             msg, addr = s.recvfrom(1024)
             udp_addr = addr
+        received += 1
+        print "%s:%d connected"%addr
 
-        sent=0
-        received=0
+        if len(msg) != 0:
+            print("<<<<<<<<<<<<<<<<<<")
+            print(msg)
+            ca_process_msg(msg,addr)
+            rtp_fake()
 
         while True:
-            if len(msg) != 0:
-                print("<<<<<<<<<<<<<<<<<<")
-                print(msg)
-                received += 1
-                ca_process_msg(msg,addr)
-                rtp_fake()
-                msg = ""
-            else:
-                if udp_mode:
-                    c.close()
-                else:
-                    s.close()
-                break
-
             msg_need_send = read_block("ca %d"%(sent))
-            if len(msg_need_send) == 0:
-                msg_need_send = read_block("ca %d@%d"%(sent,received))
+            while len(msg_need_send) == 0 and at_received <= received:
+                print "trying read block: ca %d@%d"%(sent, at_received-1)
+                msg_need_send = read_block("ca %d@%d"%(sent, at_received-1))
+                at_received += 1
+            print (sent, received, at_received, len(msg_need_send))
 
             if len(msg_need_send) != 0:
                 msg_need_send = ca_msg_preprocess(msg_need_send)
@@ -203,21 +202,53 @@ def run_ca():
                     s.sendto(msg_need_send, addr)
                 else:
                     c.send(msg_need_send)
+                    time.sleep(1) #make sure gw received data
                 sent += 1
                 continue
 
             if udp_mode:
                 msg, addr = s.recvfrom(1024)
                 if addr != udp_addr: # new gw connected, reset state
-                    sent=0
-                    received=0
+                    sent = 0
+                    received = 0
+                    at_received = received
                     udp_addr = addr
+                    print "%s:%d connected"%addr
             else:
                 msg = c.recv(1024)
+            if len(msg) != 0:
+                received += 1
+                print("<<<<<<<<<<<<<<<<<<")
+                print(msg)
+                ca_process_msg(msg,addr)
+                rtp_fake()
+            else:
+                if not udp_mode:
+                    c.close()
+                print "connection closed"
+                break
 
+def gw_process_msg(message):
+    global rtp_socket_list
+    global gw_addr
+    global ca_addr
 
-def gw_process_msg(msg):
-    pass
+    rtp_dst_addr = get_rtp_addr(message)
+    if not rtp_dst_addr:
+        return
+    if dst_addr != ca_addr:
+        print "Warnning: %s!=%s"%(dst_addr, ca_addr)
+
+    rtp_ports = get_rtp_ports(message)
+    for rtp_port in rtp_ports:
+        s_rtp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+        s_rtp.bind((gw_addr,0))
+        rtp_socket_list.append((s_rtp,(rtp_dst_addr, rtp_port)))
+        rtcp_port += 1
+        s_rtcp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+        s_rtcp.bind((gw_addr,0))
+        rtp_socket_list.append((s_rtcp,(rtp_dst_addr, rtcp_port)))
+
 
 def run_gw():
     global ca_addr
@@ -243,12 +274,16 @@ def run_gw():
 
     sent = 0
     received = 0
+    at_received = received
 
     while True:
         msg_need_send = read_block("gw %d"%(sent))
-        if len(msg_need_send) == 0:
-            msg_need_send = read_block("gw %d@%d"%(sent,received))
+        while len(msg_need_send) == 0 and at_received <= received:
+            print "trying read block: gw %d@%d"%(sent, at_received-1)
+            msg_need_send = read_block("gw %d@%d"%(sent,at_received-1))
+            at_received += 1
 
+        print (sent, received, at_received, len(msg_need_send))
         if len(msg_need_send) != 0:
             msg_need_send = gw_msg_preprocess(msg_need_send)
             print(">>>>>>>>>>>>>>>>>>")
@@ -257,6 +292,7 @@ def run_gw():
                 s.sendto(msg_need_send, address)
             else:
                 s.send(msg_need_send)
+                time.sleep(1) #make sure ca received data
             sent += 1
             continue
 
@@ -266,8 +302,10 @@ def run_gw():
             print(msg)
             received += 1
             gw_process_msg(msg)
+            rtp_fake()
         else:
             s.close()
+            print "connection closed"
             break
 
 def test_reset():
